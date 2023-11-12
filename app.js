6062e2c50746e7c111061ffcfdc082c918080e10
10Nikus <kijasnikodem@gmail.com>
2023-11-12 12:43:25 +0100

    First version of to do list

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const listItem = ["Tidy my room", "Do homework", "Go running"];
+// Define a route to render the form
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { list: listItem });
+});
+
+// Define a route to handle form submission
+app.post("/submit", (req, res) => {
+  if (req.body.todo) {
+    listItem.unshift(req.body.todo);
+  }
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const { index } = req.body;
+  if (index !== undefined) {
+    listItem.splice(index, 1);
+  }
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {
+  req.class;
+  res.redirect("/");
+});
+
+app.post("/submitEdit", (req, res) => {
+  const index = req.body.index;
+  const newText = req.body.todo;
+  if (index !== undefined && newText !== undefined) {
+    listItem[index] = newText;
+  }
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
